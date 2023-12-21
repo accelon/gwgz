d06bf7b4eb5a737920d0b5cf1166a5e64b54a517
yap cheah shen <yapcheahshen@gmail.com>
2023-12-21 21:47:28 +0800

    add n

--- src/appstore.js
+++ src/appstore.js
@@ -2,7 +2,7 @@ export * from 'accelon23/src/store.js';
 import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
 
 _ACC23.CacheName='v1::gwgz',
-_ACC23.AppVer='23.12.12';
+_ACC23.AppVer='23.12.21';
 _ACC23.AppName='gwgz'
 _ACC23.AppTitle='古文觀止'
 _ACC23.allptks=['gwgz','gwgz-yfl'];
